2f2db99ec95562f73a96879374f5e0a9f0319565
KinzataDev <Kinzata@KinzataDev.com>
2013-12-13 23:18:37 -0600

    Add player object spawner

---
+++ Assets/Scripts/SpawnObjectWithVelocity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjectWithVelocity : MonoBehaviour {
+
+	public Vector3 cannonVelocity = new Vector3(50,0,0);
+	public GameObject objectToSpawn;
+	private bool spawned = false;
+
+	void Update()
+	{
+		if( !spawned && Input.GetMouseButtonDown(0) )
+		{
+			this.Spawn();
+		}
+	}
+
+	void Spawn()
+	{
+		GameObject newObject = Instantiate( objectToSpawn, this.gameObject.transform.position, Quaternion.identity ) as GameObject;
+		newObject.rigidbody.velocity = cannonVelocity;
+		spawned = true;
+	}
+
+}
