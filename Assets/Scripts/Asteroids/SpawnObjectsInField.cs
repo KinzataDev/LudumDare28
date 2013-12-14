f9c041202bb05bedacce20f5e3764da0e8fe688b
KinzataDev <Kinzata@KinzataDev.com>
2013-12-14 00:52:40 -0600

    Add asteroid spawner script
    Spawns a list of objects in a defined field

---
+++ Assets/Scripts/Asteroids/SpawnObjectsInField.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjectsInField : MonoBehaviour {
+
+	public GameObject[] objectsToSpawn;
+
+	public float width = 0;
+	public float height = 0;
+	public float depth = 0;
+	public bool spawning = true;
+
+	public float time_between_spawns = 3;
+	public float spawn_per_cycle = 5;
+
+	public float max_scale = 3;
+	public float min_scale = 0.5f;
+
+    private float time_for_spawn = 0;
+	private int max_spawn_attempts = 5;
+
+	// Update is called once per frame
+	void Update () {
+		time_for_spawn += Time.deltaTime;
+
+		if( time_for_spawn >= time_between_spawns )
+		{
+			time_for_spawn = 0;
+			Spawn();
+		}
+	}
+
+	private void Spawn() {
+		int attempts = 0;
+		for( int i=0; i < spawn_per_cycle; i++ ) {
+			attempts = 0;
+			while( attempts < max_spawn_attempts ) {
+				attempts++;
+
+				float x = (Random.value * width) - (width * 0.5f);
+				float y = (Random.value * height) - (height * 0.5f);
+				float z = (Random.value * depth) - (depth * 0.5f);
+				Vector3 point = new Vector3(x,y,z) + transform.position;
+
+				GameObject spawnMe = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+
+				GameObject newObj = Instantiate(spawnMe,point,Quaternion.identity) as GameObject;
+				float scale = Random.Range(min_scale, max_scale);
+				newObj.transform.localScale *= scale;
+
+				attempts = max_spawn_attempts;
+			}
+		}
+
+	}
+	void OnDrawGizmos()
+	{
+		Vector3 position = transform.position;
+		Gizmos.DrawWireCube(position, new Vector3(width,height,depth));
+	}
+}
