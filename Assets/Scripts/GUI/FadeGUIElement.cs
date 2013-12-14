47add39ff80f745d8100c945f9714ba6f6088edb
KinzataDev <Kinzata@KinzataDev.com>
2013-12-14 16:13:49 -0600

    Add ice on the HUD

---
+++ Assets/Scripts/GUI/FadeGUIElement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeGUIElement : MonoBehaviour {
+
+	public float initial_opacity = 0f;
+	public float to_alpha;
+	private float current_alpha;
+
+	// Use this for initialization
+	void Start () {
+		Color currentColor = guiTexture.color;
+		currentColor.a = initial_opacity;
+		guiTexture.color = currentColor;
+
+		current_alpha = initial_opacity;
+	}
+
+	void Update() {
+
+		if( to_alpha == current_alpha ) { return; }
+
+		current_alpha = Mathf.Lerp(current_alpha, to_alpha, Time.deltaTime / 10000f);
+
+		Color currentColor = guiTexture.color;
+		currentColor.a = current_alpha;
+		guiTexture.color = currentColor;
+	}
+
+
+}
--- Assets/Scripts/Player/KeyboardMovement.cs
+++ Assets/Scripts/Player/KeyboardMovement.cs
@@ -3,21 +3,45 @@ using System.Collections;
 
 public class KeyboardMovement : MonoBehaviour {
 
-	public float force = 2.0f;
+	public float base_force = 2.0f;
+	public float base_fuel_usage = 1.0f;
+	private FuelCanister fuel;
+
+	void Start() {
+		fuel = gameObject.GetComponent<FuelCanister>();
+	}
 
 	// Update is called once per frame
 	void Update () {
+		float delta = Time.deltaTime;
+		float fuel_usage = base_fuel_usage;
+		float force = base_force;
+
+		bool expulsion = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+		if( expulsion ) {
+			fuel_usage *= 5;
+			force *= 6;
+		}
+
 		if( Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) ) {
-			gameObject.rigidbody.AddForce(new Vector3(-force,0,0));
+			if( fuel.deplete(fuel_usage * delta) ) {
+				gameObject.rigidbody.AddForce(new Vector3(-force,0,0));
+			}
 		}
 		if( Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) ) {
-			gameObject.rigidbody.AddForce(new Vector3(force,0,0));
+			if( fuel.deplete(fuel_usage * delta) ) {
+				gameObject.rigidbody.AddForce(new Vector3(force,0,0));
+			}
 		}
 		if( Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) ) {
-			gameObject.rigidbody.AddForce(new Vector3(0,force,0));
+			if( fuel.deplete(fuel_usage * delta) ) {
+				gameObject.rigidbody.AddForce(new Vector3(0,force,0));
+			}
 		}
 		if( Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ) {
-			gameObject.rigidbody.AddForce(new Vector3(0,-force,0));
+			if( fuel.deplete(fuel_usage * delta) ) {
+				gameObject.rigidbody.AddForce(new Vector3(0,-force,0));
+			}
 		}
 	}
 }
