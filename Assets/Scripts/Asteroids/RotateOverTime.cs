728e2de7e37feb721f986cc0a91e616532ed7e69
KinzataDev <Kinzata@KinzataDev.com>
2013-12-13 23:17:41 -0600

    Add rotate over time script

---
+++ Assets/Scripts/Asteroids/RotateOverTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateOverTime : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+	public Vector3 rotationVector;
+
+	void Start() {
+		rotationVector = Random.insideUnitSphere;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(rotationVector * Time.deltaTime * speed);
+	}
+}
