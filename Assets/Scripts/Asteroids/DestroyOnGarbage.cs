e2a71b56973598a242f23711c1b795acaee671bd
KinzataDev <Kinzata@KinzataDev.com>
2013-12-14 00:51:59 -0600

    Add asteroid cleanup behind the player

---
+++ Assets/Scripts/Asteroids/DestroyOnGarbage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnGarbage : MonoBehaviour {
+
+	void OnTriggerEnter(Collider obj)
+	{
+		if(obj.gameObject.tag.Equals("GarbageCollector") )
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
