f69fc21a8cfc0c4ca616d21f0946dd0d84e1f4eb
KinzataDev <Kinzata@KinzataDev.com>
2013-12-14 11:00:10 -0600

    Increase asteroid cleanup capability, add better asteroid spawning

---
+++ Assets/Scripts/Player/MoveCameraWithMouse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCameraWithMouse : MonoBehaviour {
+
+	public float rotationSpeedFactor = 1f;
+
+	private float h_rot = 0;
+	private float v_rot = 0;
+
+	// Update is called once per frame
+	void Update () {
+		v_rot = Input.GetAxis("Mouse X") * rotationSpeedFactor;
+		h_rot = Input.GetAxis("Mouse Y") * rotationSpeedFactor;
+		transform.Rotate(-h_rot,v_rot,0);
+	}
+}
