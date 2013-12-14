ae3a60fccfb889e0844f9674e3b8c0c7f2bc7ba7
KinzataDev <Kinzata@KinzataDev.com>
2013-12-14 16:15:12 -0600

    Add Fuel Bar functionality
    The fuel bar fades out over use.

---
+++ Assets/Scripts/GUI/FuelBarGUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuelBarGUI : MonoBehaviour {
+
+	public GUITexture[] bars;
+
+	// Use this for initialization
+	void Start () {
+		Rect rect = guiTexture.pixelInset;
+		float width = rect.width;
+		float offset = (width / (bars.Length + 1)) + 1.8f;
+		for( int i=0; i < bars.Length; i++ ) {
+			Rect bar_rect = bars[i].guiTexture.pixelInset;
+			bar_rect.x = offset * (i+0.5f) - (width / 2);
+			bars[i].guiTexture.pixelInset = bar_rect;
+		}
+	}
+
+	void OnEnable()
+	{
+		FuelCanister.OnFuelUsed += UpdateGUI;
+	}
+
+	void OnDisable()
+	{
+		FuelCanister.OnFuelUsed -= UpdateGUI;
+	}
+
+	void UpdateGUI(float fuel_left)
+	{
+		int bar_num = Mathf.FloorToInt((fuel_left / FuelCanister.INITIAL_FUEL) * bars.Length);
+		float fade = fuel_left % 10;
+
+		Color current_color = bars[bar_num].guiTexture.color;
+		current_color.a = fade;
+		bars[bar_num].guiTexture.color = current_color;
+	}
+}
---
+++ Assets/Scripts/Player/FuelCanister.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuelCanister : MonoBehaviour {
+
+	public delegate void FuelUsed(float fuel_left);
+	public static event FuelUsed OnFuelUsed;
+	public const float INITIAL_FUEL = 1000f;
+
+	private float fuel;
+
+	void Start() {
+		fuel =  INITIAL_FUEL;
+	}
+
+	public bool deplete(float amount)
+	{
+		if( amount > fuel)
+		{
+			fuel = 0;
+			Debug.Log("Fuel Empty!!!");
+			return false;
+		}
+
+		Debug.Log("Fuel at: " + fuel);
+		fuel -= amount;
+
+		if(OnFuelUsed != null )
+		{
+			OnFuelUsed(fuel);
+		}
+
+		return true;
+	}
+
+}
