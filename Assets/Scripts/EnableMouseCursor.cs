a7eb54b986741e9be7e18c65d33a84e84a07bce5
KinzataDev <Kinzata@KinzataDev.com>
2013-12-15 15:48:18 -0600

    Add script to simply enable and disable the mouse

---
+++ Assets/Scripts/EnableMouseCursor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableMouseCursor : MonoBehaviour {
+
+	public bool enabled = true;
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = enabled;
+	}
+
+}
