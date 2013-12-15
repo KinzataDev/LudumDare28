2d4a09d4b4fb76603604adf0dda08353d66be7bd
KinzataDev <Kinzata@KinzataDev.com>
2013-12-14 19:25:39 -0600

    Modify sideways movement, lock at top speed
    Prevents players from moving sideways faster than they can move forward.

--- Assets/Scripts/Player/KeyboardMovement.cs
+++ Assets/Scripts/Player/KeyboardMovement.cs
@@ -5,6 +5,10 @@ public class KeyboardMovement : MonoBehaviour {
 
 	public float base_force = 2.0f;
 	public float base_fuel_usage = 1.0f;
+
+	public float max_x_velocity = 100f;
+	public float max_y_velocity = 100f;
+
 	private FuelCanister fuel;
 
 	void Start() {
@@ -16,6 +20,8 @@ public class KeyboardMovement : MonoBehaviour {
 		float delta = Time.deltaTime;
 		float fuel_usage = base_fuel_usage;
 		float force = base_force;
+		Vector3 current_velocity = gameObject.rigidbody.velocity;
+		Debug.Log("Current Velocity: " + current_velocity );
 
 		bool expulsion = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 		if( expulsion ) {
@@ -24,22 +30,28 @@ public class KeyboardMovement : MonoBehaviour {
 		}
 
 		if( Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) ) {
-			if( fuel.deplete(fuel_usage * delta) ) {
+			if(   current_velocity.x > -max_x_velocity 
+			   && fuel.deplete(fuel_usage * delta) ) 
+			{
 				gameObject.rigidbody.AddForce(new Vector3(-force,0,0));
 			}
 		}
 		if( Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) ) {
-			if( fuel.deplete(fuel_usage * delta) ) {
+			if(   current_velocity.x < max_x_velocity
+			   && fuel.deplete(fuel_usage * delta) ) 
+			{
 				gameObject.rigidbody.AddForce(new Vector3(force,0,0));
 			}
 		}
 		if( Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) ) {
-			if( fuel.deplete(fuel_usage * delta) ) {
+			if(   current_velocity.y < max_y_velocity 
+			   && fuel.deplete(fuel_usage * delta) ) {
 				gameObject.rigidbody.AddForce(new Vector3(0,force,0));
 			}
 		}
 		if( Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ) {
-			if( fuel.deplete(fuel_usage * delta) ) {
+			if(   current_velocity.y > -max_y_velocity 
+			   && fuel.deplete(fuel_usage * delta) ) {
 				gameObject.rigidbody.AddForce(new Vector3(0,-force,0));
 			}
 		}
