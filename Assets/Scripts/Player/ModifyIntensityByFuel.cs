aef91472415d4aeae3123639ee5dd0275fd72fa6
KinzataDev <Kinzata@KinzataDev.com>
2013-12-15 15:48:46 -0600

    Modify ship light to get brighter with less fuel

---
+++ Assets/Scripts/Player/ModifyIntensityByFuel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifyIntensityByFuel : MonoBehaviour {
+
+	public float max_intensity = 0.9f;
+	public float min_intensity = 0.25f;
+	private float base_intensity;
+
+	void Start() {
+		base_intensity = max_intensity - min_intensity;
+	}
+
+	void OnEnable() {
+		FuelCanister.OnFuelUsed += HandleOnFuelUsed;
+		DeathOnCollision.OnPlayerDeath += HandleOnPlayerDeath;
+	}
+
+	void HandleOnPlayerDeath ()
+	{
+		light.flare = null;
+	}
+
+	void HandleOnFuelUsed (float fuel_left)
+	{
+		float percent = 1 - (fuel_left / FuelCanister.INITIAL_FUEL);
+		light.intensity = base_intensity * percent + min_intensity;
+	}
+
+	void OnDisable() {
+		FuelCanister.OnFuelUsed -= HandleOnFuelUsed;
+		DeathOnCollision.OnPlayerDeath -= HandleOnPlayerDeath;
+	}
+}
